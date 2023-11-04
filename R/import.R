55cb7c42b59559c038349e5ab72aed6f74b1f877
Samuel Aeschbach <samuel.aeschbach@gmail.com>
2023-11-04 20:23:27 +0100

    add generalized import function ar_import to handle all types of data

--- R/import.R
+++ R/import.R
@@ -1,5 +1,169 @@
 #' Title
 #'
+#' @param data
+#' @param participant_id
+#' @param response_col
+#' @param cue_type
+#' @param cue_col
+#' @param cue_manual
+#' @param response_level
+#' @param participant_attributes
+#' @param cue_attributes
+#' @param response_attributes
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
+#'
+ar_import <- function(data, participant_id, response_col, cue_type,
+                      cue_col = NULL,
+                      cue_manual = NULL,
+                      response_level = NULL,
+                      participant_attributes = NULL,
+                      cue_attributes = NULL,
+                      response_attributes = NULL) {
+  # check argumets -------------------------------------------------------------
+
+  # data
+  chk::chk_data(data)
+
+  # participant_id
+  chk::chk_character(participant_id)
+
+  # response_col
+  chk::chk_character(response_col)
+
+  # cue_type
+  chk::chk_character(cue_type)
+  chk::chk_subset(cue_type, c("manual", "col"))
+
+  # cue_col
+  if (cue_type == "col") {
+    chk::chk_character(cue_col)
+    chk::chk_subset(cue_col, names(data))
+  } else {
+    chk::chk_null(cue_col)
+  }
+
+  # cue_manual
+  if (cue_type == "manual") {
+    chk::chk_character(cue_manual)
+  } else {
+    chk::chk_null(cue_manual)
+  }
+
+  # response_level
+  if (!is.null(response_level)) {
+    chk::chk_character(response_level)
+    chk::chk_subset(response_level, names(data))
+  }
+
+  # participant_attributes
+  if (!is.null(participant_attributes)) {
+    chk::chk_character(participant_attributes)
+    chk::chk_subset(participant_attributes, names(data))
+  }
+
+  # cue_attributes
+  if (!is.null(cue_attributes)) {
+    chk::chk_character(cue_attributes)
+    chk::chk_subset(cue_attributes, names(data))
+  }
+
+  # response_attributes
+  if (!is.null(response_attributes)) {
+    chk::chk_character(response_attributes)
+    chk::chk_subset(response_attributes, names(data))
+  }
+
+  # participants ---------------------------------------------------------------
+
+  # all use-cases
+  participants <- data %>%
+    dplyr::select(dplyr::all_of(c(participant_id, participant_attributes))) %>%
+    dplyr::distinct()
+
+  # handle participant_attributes
+  if(length(names(participants)) > 1) {
+    names(participants) <- c("p_id", paste0("p_attr_", names(participants)[-1]))
+  } else {
+    names(participants) <- c("p_id")
+  }
+
+  # cues -----------------------------------------------------------------------
+
+  switch (cue_type,
+          "manual" = {
+            # cue is a string, single cue case, e.g. AInet
+            cues <- tibble::tibble(c_cue = cue_manual)
+          },
+          "col" = {
+            # cue is a variable name in data
+            if(is.null(response_level)) {
+              # get cues from cue variable
+              cues <- data %>%
+                dplyr::select(dplyr::all_of(c(cue_col, cue_attributes))) %>%
+                dplyr::distinct()
+            } else {
+              # get cues from cue variable where response_level is 1
+              cues <- data %>%
+                dplyr::filter(eval(parse(text = response_level)) == 1) %>%
+                dplyr::select(dplyr::all_of(c(cue_col, cue_attributes))) %>%
+                dplyr::distinct()
+            }
+          }
+  )
+
+  # handle cue_attributes
+  if(length(names(cues)) > 1) {
+    names(cues) <- c("c_cue", paste0("c_attr_", names(cues)[-1]))
+  } else {
+    names(cues) <- c("c_cue")
+  }
+
+  # responses ------------------------------------------------------------------
+
+  switch (cue_type,
+          "manual" = {
+            responses <- data %>%
+              dplyr::mutate(r_cue = cue_manual, r_level = 1) %>%
+              dplyr::select(dplyr::all_of(c(participant_id, "r_cue", response_col, "r_level", response_attributes)))
+          },
+          "col" = {
+            if (is.null(response_level)) {
+              responses <- data %>%
+                dplyr::mutate(r_level = 1) %>%
+                dplyr::select(dplyr::all_of(c(participant_id, cue_col, response_col, "r_level", response_attributes)))
+            } else {
+              responses <- data %>%
+                dplyr::select(dplyr::all_of(c(participant_id, cue_col, response_col, response_level, response_attributes)))
+
+            }
+          })
+
+  # handle response_attributes
+  if(length(names(responses)) > 4) {
+    names(responses) <- c("p_id", "r_cue", "r_response", "r_level", paste0("r_attr_", names(responses)[-(1:4)]))
+  } else {
+    names(responses) <- c("p_id", "r_cue", "r_response", "r_level")
+  }
+
+  # compose associatoR object --------------------------------------------------
+
+  output <- list(participants = participants,
+                 cues = cues,
+                 responses = responses)
+  class(output) <- "associatoR"
+
+  output
+
+}
+
+#' ar_import_single_cue_single_response_level
+#'
 #' @param data a \code{data.frame} or \code{tibble} containing the association data.
 #' @param cue a \code{character} scalar to specify a cue that was used for all responses.
 #' @param participant_id a \code{character} scalar of the variable name in \code{data} identifying individual participants.
@@ -67,7 +231,7 @@ ar_import_single_cue_single_response_level <- function(data,
 
 }
 
-#' Title
+#' ar_import_multiple_cues_single_response_level
 #'
 #' @param data
 #' @param cue
@@ -145,8 +309,7 @@ ar_import_multiple_cues_single_response_level <- function(data,
 
 }
 
-
-#' Title
+#' ar_import_single_cue_multiple_response_levels
 #'
 #' @param data
 #' @param cue
