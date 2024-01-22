30447bfcd0b934d43e24ea48dd2d14cc0ad7f8d5
Samuel Aeschbach <samuel.aeschbach@gmail.com>
2024-01-22 18:19:05 +0100

    add 'intelligence' data set and docs

--- R/data.R
+++ R/data.R
@@ -1,3 +1,23 @@
+#' GPT-4-generated free associations to 'intelligence'
+#'
+#' Inspect and use the data by calling \code{intelligence} in your R session.
+#'
+#' @format ## `intelligence`
+#' A tibble with 29,820 rows and 7 coulumns:
+#' \describe{
+#'  \item{participant_id}{Unique id for each participant.}
+#'  \item{gender}{Gender of the participant.}
+#'  \item{education}{Education level of the participant}
+#'  \item{cue}{Cue word for this trial.}
+#'  \item{response}{Response given by the participant in this trial.}
+#'  \item{response_position}{Position of the response given (1. response to cue, 2. response to cue, etc.).}
+#'  \item{response_level}{Response level of the trial. (1: first-level response to the cue 'intelligence', 2: second-level response to first-level responses of the same participant as a cue).}
+#' }
+#'
+#' @references Aeschbach, S., Mata, R., Wulff, D. U. (in preparation). associatoR.
+#' @source \url{https://doi.org}
+"intelligence"
+
 #' AI association data
 #'
 #' \code{ar_import} processes R data and generates an associatoR object
